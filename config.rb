7b910d2693d6dabf7358d33ea57e5ff48a02a8d2
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2014-12-06 23:44:05 +0200

    Used double quotes, in order to be consistent with the rest of the code in the configuration file.

--- config.rb
+++ config.rb
@@ -1,6 +1,6 @@
 require "redcarpet"
 require "stringex"
-require 'builder'
+require "builder"
 
 set :markdown_engine , :redcarpet
 set :markdown        , :fenced_code_blocks => true, :smartypants => true
